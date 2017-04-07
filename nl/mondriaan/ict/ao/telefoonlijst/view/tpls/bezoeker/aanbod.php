07b7cb059e8b3a256c5fe7c66a5cceb275fe59e1
sam <sam@10.163.33.44>
2017-04-07 11:34:12 +0200

    DANK AF

--- nl/mondriaan/ict/ao/telefoonlijst/view/tpls/bezoeker/aanbod.php
+++ nl/mondriaan/ict/ao/telefoonlijst/view/tpls/bezoeker/aanbod.php
@@ -2,7 +2,54 @@
 include 'includes/header.php';
 include 'includes/menu.php';?>
         <section id='content'>
-            
-           
+            <table>
+                <tr>
+                    <td>
+                        <img src="img/stootzaktraining.jpg" alt="Stootzak Training" style="width:100%;"/>
+                    </td>
+                    <td>
+                        <div id="text">
+                        <h2>Stootzak Training</h2>
+                        <p>Stootzak training is een les waarbij gebruik wordt gemaakt van de stootzak.
+                           Op de stootzakken worden verschillende klassieke boks en traptechnieken aangeleerd en geoefend.
+                           Onze club beschikt over 10 stootzakken waar u op u eigen niveau kunt trainen en niet te maken 
+                           heeft met een tegenstander waardoor jezelf grote invloed heb op de intensiteit van de training,
+                           mede hierdoor kan je alle stress en negatieve energie botvieren op de stootzak.
+                        </p>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="img/trainingkickboksen.jpg" alt="Kickboks Training" style="width:100%; margin-top:20px;"/>
+                    </td>
+                    <td>
+                        <div id="text">
+                        <h2>Kickboks Training</h2>
+                        <p>Je traint kracht, conditie, techniek, uithouding, karakter en functie.
+                            Boks-intervaltrainingen zijn de meest effectieve trainingen, intensieve workouts,
+                            in plaats van uren lang op een loopband of crosstrainer staan.
+                        </p>
+                        </div>
+                    </td>
+                </tr>
+                  <tr>
+                    <td>
+                        <img src="img/mma.jpg" alt="MMA Training" style="width:100%; margin-top:20px;"/>
+                    </td>
+                    <td>
+                        <div id="text">
+                        <h2>MMA</h2>
+                        <p>Deze combinatie heeft als doel het vormen van de meest effectieve vechtsport voor een in theorie vrij gevecht.
+                            Er zijn diverse varianten van Mixed Martial Arts en verschillende organisaties, ieder met zijn eigen kenmerken en regels,
+                            waaronderfree fight (het vrije gevecht), vale tudo (Braziliaans Mixed Martial Arts) en cage fight (kooigevecht).
+                            Het grote verschil met "gangbare" vechtsporten is dat veel geoorloofd is, van stoot- en traptechnieken, 
+                            elleboog- en knietechnieken, tot klem- en wurgtechnieken.
+                        </p>
+                        </div>
+                    </td>
+                </tr>
+               
+            </table>
         </section>
 <?php include 'includes/footer.php';
