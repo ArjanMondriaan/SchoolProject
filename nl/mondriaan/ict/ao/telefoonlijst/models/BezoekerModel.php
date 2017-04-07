d9521d7c159ca66403518bb7be3d3b4957e1cfb6
yunus10 <y.gogce@mondriaanict.nl>
2017-04-07 14:20:47 +0200

    Added controleerInloggen

--- nl/mondriaan/ict/ao/telefoonlijst/models/BezoekerModel.php
+++ nl/mondriaan/ict/ao/telefoonlijst/models/BezoekerModel.php
@@ -81,4 +81,30 @@ class BezoekerModel extends \ao\php\framework\models\AbstractModel
              return REQUEST_FAILURE_DATA_INVALID;
         }
     }
+    
+    public function controleerInloggen()
+    {
+        $gn=  filter_input(INPUT_POST, 'gn');
+        $ww=  filter_input(INPUT_POST, 'ww');
+        
+        if ( ($gn!==null) && ($ww!==null) )
+        {
+             $sql = 'SELECT * FROM `persons` WHERE `username` = :gn AND `password` = :ww';
+             $sth = $this->dbh->prepare($sql);
+             $sth->bindParam(':gn',$gn);
+             $sth->bindParam(':ww',$ww);
+             $sth->execute();
+             
+             $result = $sth->fetchAll(\PDO::FETCH_CLASS,__NAMESPACE__.'\db\Contact');
+             
+             if(count($result) === 1)
+             {   
+                 $this->startSessie();   
+                 $_SESSION['gebruiker']=$result[0];
+                 return REQUEST_SUCCESS;
+             }
+             return REQUEST_FAILURE_DATA_INVALID;
+        }
+        return REQUEST_FAILURE_DATA_INCOMPLETE;
+    }
 }
