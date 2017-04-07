7a14cecc88b3ae8ad01453940600fa4dd79eef8e
sam <sam@10.163.33.44>
2017-04-07 12:35:35 +0200

    SUPER FUCKING DANK G

--- nl/mondriaan/ict/ao/telefoonlijst/view/tpls/bezoeker/gedragregels.php
+++ nl/mondriaan/ict/ao/telefoonlijst/view/tpls/bezoeker/gedragregels.php
@@ -7,14 +7,16 @@ include 'includes/menu.php';?>
                 <p>Sporten volgens de algemene positieve waarden en normen en de maatregelen die daaruit voortvloeien.
                 Iedereen moet deze in acht nemen met het oog op het bewaren en bevorderen van de individuele integriteit 
                 (fysieke, psychische en seksuele), fairplay en sociale integriteit (solidariteit, diversiteit en inclusie).</p>
-               
-                <ol style=" margin-top:10px;font-size:13px;">
-                    <li>Iedereen is welkom in onze sportclub zowel de recreanten als competitie sporters. Er worden inspanningen geleverd zodat iedereen er zich thuis kan voelen.</li>
-                    <li>Er wordt op een medisch en ethisch verantwoorde manier getraind en aan wedstrijden deelgenomen (gelijkwaardigheid van kampers, …).</li>
-                    <li>De sporter wordt beschermd tegen de negatieve effecten die sport kan veroorzaken zoals stress, prestatiedruk, blessures, gezondheidsrisico’s, dopingmisbruik, intimidatie, pestgedrag, machtsmisbruik, agressie en geweld.</li>
-                    <li>Er wordt respect getoond voor anderen, zowel clubleden, tegenstrevers, trainers, scheidsrechters als supporters, ongeacht hun geslacht, culturele achtergrond, geaardheid, herkomst, sociale status of handicap.</li>
-                    <li>Sportiviteit en fair play worden hoog in het vaandel gedragen.</li>                    
-                </ol>
             </div>
+            
+            <ol style="margin-left:20px;">
+                <li>Iedereen is welkom in onze sportclub zowel de recreanten als competitie sporters. Er worden inspanningen geleverd zodat iedereen er zich thuis kan voelen.</li>
+                <li>Er wordt op een medisch en ethisch verantwoorde manier getraind en aan wedstrijden deelgenomen (gelijkwaardigheid van kampers)</li>
+                <li>De sporter wordt beschermd tegen de negatieve effecten die sport kan veroorzaken zoals stress, prestatiedruk, blessures, gezondheidsrisico’s, dopingmisbruik, intimidatie, pestgedrag, machtsmisbruik, agressie en geweld.</li>
+                <li>Er wordt respect getoond voor anderen, zowel clubleden, tegenstrevers, trainers, scheidsrechters als supporters, ongeacht hun geslacht, culturele achtergrond, geaardheid, herkomst, sociale status of handicap.</li>
+                <li>Sportiviteit en fair play worden hoog in het vaandel gedragen.</li>
+               
+            </ol>
+            
         </section>
-<?php include 'includes/footer.php';
+<?php include 'includes/footer.php';?>
