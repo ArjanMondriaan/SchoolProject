4fbbafa7e1567e0aa11e086f8aa26f19fa1e068d
yunus10 <y.gogce@mondriaanict.nl>
2017-04-12 14:43:42 +0200

    Alle activiteiten bekijken/lessen

--- nl/mondriaan/ict/ao/telefoonlijst/view/tpls/lid/includes/menu.php
+++ nl/mondriaan/ict/ao/telefoonlijst/view/tpls/lid/includes/menu.php
@@ -4,18 +4,6 @@
                     <a href="?control=lid&action=default">Home</a>
                 </li>
                 <li>
-                    <a href="?control=bezoeker&action=aanbod">Trainings aanbod</a>
-                </li>
-                <li>
-                    <a href="?control=bezoeker&action=registreren">Lid worden</a>
-                </li>
-                <li>
-                    <a href="?control=bezoeker&action=inloggen">Gedragsregels</a>
-                </li>
-                <li>
-                    <a href="?control=bezoeker&action=inloggen">Locatie & contact</a>
-                </li>
-                <li>
                     <a href="?control=lid&action=gegevensWijzigen">Profiel aanpassen</a>
                 </li>
                 <li>
