92017f28598883136acfd1d1c011d56d555596fd
ArjanMondriaan <a.kuiper@mondriaanict.nl>
2017-04-12 15:01:11 +0200

    Dank Meme 420

--- nl/mondriaan/ict/ao/telefoonlijst/view/tpls/instructeur/lessen.php
+++ nl/mondriaan/ict/ao/telefoonlijst/view/tpls/instructeur/lessen.php
@@ -7,31 +7,24 @@ include 'includes/menu.php';?>
                 <img src="img/foto3.jpg"/>
             </figure>
             <section >
-        <table >
-            <caption>Dit zijn alle activiteiten van het kartcentrum</caption>
+        <table style="margin-left:200px;">
+            <caption>Dit zijn alle trainingen</caption>
             <thead>
                 <tr>
-                    <td>ID</td>
-                    <td>Tijdsduur</td>
                     <td>Datum</td>
+                    <td>Begin tijd</td>
                     <td>Locatie</td>
                     <td>Max personen</td>
-                    <td>personid</td>
-                    <td>trainingid</td>
                     <td colspan="2">acties</td>
                 </tr>
             </thead>
-            <tbody>
+            <tbody >
                 <?php foreach($lessen as $les):?>
                 <tr>
-                    
-                    <td><?= $les->getId();?></td>
-                    <td><?= $les->getTime();?></td>
                     <td><?= $les->getDate();?></td>
+                    <td><?= $les->getTime();?></td>
                     <td><?= $les->getLocation();?></td>
                     <td><?= $les->getMaxpersons();?></td>
-                    <td><?= $les->getPersonid();?></td>
-                    <td><?= $les->getTrainingid();?></td>
                     
                     <td title="bewerk de gegevens van deze activiteit"><a href='?control=Instructeur&action=editSa&id=<?= $les->getId();?>'><img src="img/bewerk.png"></a></td>
                     <td title="verwijder deze activiteit is definitief"><a href='?control=Instructeur&action=deleteSa&id=<?= $les->getId();?>'><img src="img/verwijder.png"></a></td>
